0fc40b45e5e76d7210a07dd56ff660b0d3713c0e
Franco Alejandro Cabezas Poblete <facabeza@alumnos.inf.utfsm.cl>
2021-12-05 23:25:01 -0300

    Actualizacion de vistas, organización de css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,9 +3,3 @@
 
 // Write your JavaScript code.
 
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-    const inputValue = searchInput.value;
-    alert(inputValue);
-});
